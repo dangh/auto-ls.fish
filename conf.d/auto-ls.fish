c5edbf92323f7a0b474e150d3168372dd2123498
Dang <huynhminhdang@gmail.com>
2020-12-31 20:37:12 +0800

    prevent ls on start

--- conf.d/auto-ls.fish
+++ conf.d/auto-ls.fish
@@ -1,6 +1,10 @@
 if status is-interactive
   function auto-ls --on-variable PWD
+    test "$auto_ls_initialized" = "true" || return
     test -z "$auto_ls_command"; and set auto_ls_command "ls -GF"
     eval $auto_ls_command
+  end
+  function auto-ls-init --on-event fish_prompt
+    set --global auto_ls_initialized true
   end
 end
