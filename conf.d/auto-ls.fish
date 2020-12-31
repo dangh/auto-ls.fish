6b1eefa7b5ad7fb59daa2103dd92f05cd21c6385
Dang <dang@travelstop.com>
2021-01-01 04:07:09 +0800

    clean up

--- conf.d/auto-ls.fish
+++ conf.d/auto-ls.fish
@@ -1,10 +1,10 @@
 if status is-interactive
   function auto-ls --on-variable PWD
-    test "$auto_ls_initialized" = "true" || return
-    test -z "$auto_ls_command"; and set auto_ls_command "ls -GF"
-    eval $auto_ls_command
+    set --query auto_ls_initialized || return
+    set --query auto_ls_command || set --local auto_ls_command 'ls -GF'
+    $auto_ls_command
   end
   function auto-ls-init --on-event fish_prompt
-    set --global auto_ls_initialized true
+    set --global auto_ls_initialized
   end
 end
