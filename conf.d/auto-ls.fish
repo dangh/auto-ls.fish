e850cd0c4ce63d5cdcef3a5e57b7a4240abbe90b
Dang <huynhminhdang@gmail.com>
2021-01-03 04:45:33 +0800

    fix eval

--- conf.d/auto-ls.fish
+++ conf.d/auto-ls.fish
@@ -2,7 +2,7 @@ if status is-interactive
   function auto-ls --on-variable PWD
     set --query auto_ls_initialized || return
     set --query auto_ls_command || set --local auto_ls_command 'ls -GF'
-    $auto_ls_command
+    eval $auto_ls_command
   end
   function auto-ls-init --on-event fish_prompt
     set --global auto_ls_initialized
