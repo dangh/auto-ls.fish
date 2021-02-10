497fd80b934e4990eb46314b7bffca09572cd5e2
Dang <dang@travelstop.com>
2021-02-10 23:29:47 +0800

    only ls if the directory is small enough

--- README.md
+++ README.md
@@ -17,3 +17,11 @@ For example, to show hidden files/directories by default:
 ```sh
 set -U auto_ls_command 'ls -AGF'
 ```
+
+By default it will only run `ls` if there are less than 30 files in the directory. This behaviour is to prevent `ls` hanging when `cd` to a directory with a very large number of files.
+
+To change the limit number, set the variable as:
+
+```sh
+set -U auto_ls_limit 100
+```
--- conf.d/auto-ls.fish
+++ conf.d/auto-ls.fish
@@ -2,6 +2,12 @@ status is-interactive || exit
 
 function auto-ls --on-event fish_prompt
   function auto-ls --on-variable PWD
+    set --query auto_ls_limit || set --local auto_ls_limit 30
+    set --local count (count *)
+    test $count -le $auto_ls_limit || begin
+      echo Too many files to list! \((set_color yellow)$count(set_color normal) files\)
+      return
+    end
     test -n "$auto_ls_command" || set --local auto_ls_command 'ls -GF'
     eval $auto_ls_command
   end
