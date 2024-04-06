f4e042402086bc6278c17ef3244f6840b4428e6c
Dang <huynhminhdang@gmail.com>
2024-04-06 21:20:53 +0800

    only run command once after prompt

--- conf.d/auto-ls.fish
+++ conf.d/auto-ls.fish
@@ -1,8 +1,8 @@
 status is-interactive || exit
 
-function auto-ls -e fish_prompt
-    function auto-ls -v PWD
-        set -q auto_ls_limit || set -l auto_ls_limit 30
+function auto-ls -v PWD
+    set -q _auto_ls_enabled && begin
+        set -q auto_ls_limit || set -l auto_ls_limit 50
         set -l count (count *)
         test $count -le $auto_ls_limit || begin
             echo Too many files to list! \((set_color yellow)$count(set_color normal) files\)
@@ -11,4 +11,9 @@ function auto-ls -e fish_prompt
         test -n "$auto_ls_command" || set -l auto_ls_command 'ls -GF'
         eval $auto_ls_command
     end
+    set -e _auto_ls_enabled
+end
+
+function _enable_auto_ls -e fish_prompt
+    set -g _auto_ls_enabled
 end
